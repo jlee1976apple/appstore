6ac5fa1d248db556883818eccae30bd2728f8111
jlee1976devops <jlee1976apple@gmail.com>
2020-07-26 14:52:10 -0400

    updated configure database sql script.

--- appstore-web/src/main/scripts/configure-mysql.sql
+++ appstore-web/src/main/scripts/configure-mysql.sql
@@ -1,7 +1,7 @@
 -- docker run -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d leejon_mysql
 
-CREATE sfg_dev;
-CREATE sfg_prod;
+CREATE database sfg_dev;
+CREATE database sfg_prod;
 
 CREATE user 'sfg_dev_user'@'localhost' IDENTIFIED BY 'root';
 CREATE user 'sfg_prod_user'@'localhost' IDENTIFIED BY 'root';
