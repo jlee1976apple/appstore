55158174dee2c21343abfaf5bf2ba862024613bc
jlee1976devops <jlee1976apple@gmail.com>
2020-07-26 15:04:36 -0400

    modified SQL script

--- appstore-web/src/main/scripts/configure-mysql.sql
+++ appstore-web/src/main/scripts/configure-mysql.sql
@@ -3,14 +3,14 @@
 CREATE database sfg_dev;
 CREATE database sfg_prod;
 
-CREATE user 'sfg_dev_user'@'localhost' IDENTIFIED BY 'root';
-CREATE user 'sfg_prod_user'@'localhost' IDENTIFIED BY 'root';
+CREATE user 'sfg_dev_user'@'10.0.0.21' IDENTIFIED BY 'root';
+CREATE user 'sfg_prod_user'@'10.0.0.21' IDENTIFIED BY 'root';
 
-GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
-GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
-GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
-GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
-GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
-GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
-GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
-GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'10.0.0.21';
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'10.0.0.21';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'10.0.0.21';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'10.0.0.21';
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'10.0.0.21';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'10.0.0.21';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'10.0.0.21';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'10.0.0.21';
