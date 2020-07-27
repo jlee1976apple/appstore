4983f0b415bfd5923e4db6715784d95e577da543
jlee1976devops <jlee1976apple@gmail.com>
2020-07-26 21:54:09 -0400

    created application.properties (dev/test/prod) and schema creation SQL script.

--- appstore-web/src/main/scripts/configure-mysql.sql
+++ appstore-web/src/main/scripts/configure-mysql.sql
@@ -1,16 +1,24 @@
 -- docker run -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d leejon_mysql
 
-CREATE database sfg_dev;
-CREATE database sfg_prod;
+CREATE DATABASE appstore_dev;
+CREATE DATABASE appstore_test;
+CREATE DATABASE appstore_prod;
 
-CREATE user 'sfg_dev_user'@'10.0.0.21' IDENTIFIED BY 'root';
-CREATE user 'sfg_prod_user'@'10.0.0.21' IDENTIFIED BY 'root';
+CREATE USER 'appstore_dev_user' IDENTIFIED BY 'appstore$1';
+CREATE USER 'appstore_test_user' IDENTIFIED BY 'appstore$1';
+CREATE USER 'appstore_prod_user' IDENTIFIED BY 'appstore$1';
 
-GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'10.0.0.21';
-GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'10.0.0.21';
-GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'10.0.0.21';
-GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'10.0.0.21';
-GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'10.0.0.21';
-GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'10.0.0.21';
-GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'10.0.0.21';
-GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'10.0.0.21';
+GRANT SELECT ON appstore_dev.* TO 'appstore_dev_user';
+GRANT INSERT ON appstore_dev.* TO 'appstore_dev_user';
+GRANT UPDATE ON appstore_dev.* TO 'appstore_dev_user';
+GRANT DELETE ON appstore_dev.* TO 'appstore_dev_user';
+
+GRANT SELECT ON appstore_test.* TO 'appstore_test_user';
+GRANT INSERT ON appstore_test.* TO 'appstore_test_user';
+GRANT UPDATE ON appstore_test.* TO 'appstore_test_user';
+GRANT DELETE ON appstore_test.* TO 'appstore_test_user';
+
+GRANT SELECT ON appstore_prod.* TO 'appstore_prod_user';
+GRANT INSERT ON appstore_prod.* TO 'appstore_prod_user';
+GRANT UPDATE ON appstore_prod.* TO 'appstore_prod_user';
+GRANT DELETE ON appstore_prod.* TO 'appstore_prod_user';
